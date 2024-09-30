fb030cab8f6f251b3ab32ec3a76a4c9a884def29
pangwoo96 <junggw96@gmail.com>
2024-09-30 15:54:54 +0900

    feature: 초기 dev 브랜치 설정 및 의존성 추가

---
+++ src/main/java/com/sparklenote/SparkleNoteApplication.java
@@ -0,0 +1,13 @@
+package com.sparklenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkleNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkleNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparklenote/SparkleNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparklenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkleNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
