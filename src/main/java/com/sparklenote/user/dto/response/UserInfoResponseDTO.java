346828b08e40287eff89accd9bfcac41b6eecce5
pangwoo96 <156070047+pangwoo96@users.noreply.github.com>
2024-10-29 17:35:35 +0900

    Refactor/roll UserInfoResponseDTO 누락 (클래스 추가 커밋) (#58)
    * feat : /user/info API 호출 시 사용자의 userId, name 반환
    
    * refactor : @RequestBody -> @RequestHeader로 변경 (보안상 더 유리함)
    
    * feat : UserInfoResponseDTO 클래스 추가

---
+++ src/main/java/com/sparklenote/user/dto/response/UserInfoResponseDTO.java
@@ -0,0 +1,16 @@
+package com.sparklenote.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponseDTO {
+
+    private Long userId;
+    private String name;
+}
